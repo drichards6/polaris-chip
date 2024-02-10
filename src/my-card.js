c4e3a12dda37f299c4e983df6dd02220a69f64bf
drichards6 <drr5489@psu.edu>
2024-02-09 22:25:59 -0500

    In class 2/8/24
    implementing <slot> and fancy

--- src/my-card.js
+++ src/my-card.js
@@ -25,8 +25,9 @@ export class MyCard extends LitElement {
     super();
     this.myTitle = "My card";
     this.img = "#";
-    this.details = "Details";
+    this.description = "Details";
     this.link = "#";
+    this.fancy = false;
   }
 
   static get styles() {
@@ -35,61 +36,100 @@ export class MyCard extends LitElement {
         display: block;
       }
 
+      :host([fancy]) .card {
+        background-color: pink;
+        border: 2px solid fuchsia;
+        box-shadow: 10px 5px 5px red;
+      }   
+
       .card.change-color {
         background-color: var(--basic-color);
       }
 
-.card {
-  background-color: #D4EFFD;
-  color: Black;
-  max-width: 400px;
-  text-align: center;
-  border: 1px solid black;
-  margin: 10px;
-  padding: 10px;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-}
+      .card {
+        background-color: #D4EFFD;
+        color: Black;
+        max-width: 400px;
+        text-align: center;
+        border: 1px solid black;
+        margin: 10px;
+        padding: 10px;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+      }
 
-.card-title {
-  font-size: 40px;
-}
+      .card-title {
+        font-size: 40px;
+      }
 
-.card-image {
-  width: 350px;
-  height: auto;
-}
+      .card-image {
+        width: 350px;
+        height: auto;
+      }
 
-.card-details {
-  font-size: 30px;
-  font-family: serif;
-}
+      .card-details {
+        font-size: 30px;
+        font-family: serif;
+      }
 
-.button {
-  color: white;
-  background-color: Black;
-  margin: 2px 2px 8px 2px;
-  padding: 5px 20px ;
-  border-radius: 15%;
-  font-size: 25px;
-}
+      .button {
+        color: white;
+        background-color: Black;
+        margin: 2px 2px 8px 2px;
+        padding: 5px 20px ;
+        border-radius: 15%;
+        font-size: 25px;
+      }
 
-.button:focus,
-.button:hover {
-  background-color: green;
-  color: white;
-}
+      .button:focus,
+      .button:hover {
+        background-color: green;
+        color: white;
+      }
 
-  `;
-}
+      details summary {
+      text-align: left;
+      font-size: 20px;
+      padding: 8px 0;
+      }
+
+      details[open] summary {
+        font-weight: bold;
+      }
+  
+      details div {
+        border: 2px solid black;
+        text-align: left;
+        padding: 8px;
+        height: 70px;
+        overflow: auto;
+      }
+
+    `;
+  }
+
+  openChanged(e) {
+    console.log(e.newState);
+    if (e.newState === "open") {
+      this.fancy = true;
+    }
+    else {
+      this.fancy = false;
+    }
+  }
 
   render() {
     return html`
         <div class="card">
             <h1 class="card-title">${this.myTitle}</h1>
             <img class="card-image" src="${this.img}">
-            <p class="card-details">${this.details}</p>
+            <details ?open="${this.fancy}" @toggle="${this.openChanged}">
+              <summary>Description</summary>
+              <div>
+                <slot>${this.description}</slot>
+              </div>
+            </details>
             <a href="${this.link}" rel="noopener noreferrer">
               <button class="button">Details</button>
             </a>
@@ -97,12 +137,14 @@ export class MyCard extends LitElement {
     `; 
   }
 
+
   static get properties() {
     return {
       myTitle: { type: String, attribute: "my-title" },
       img: { tpe: String },
-      details: { tpe: String },
+      description: { tpe: String },
       link: { tpe: String },
+      fancy: { type: Boolean, reflect: true },
     };
   }
 }
