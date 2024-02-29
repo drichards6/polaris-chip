e07e266decef3723fa00f8859b2b68f77c5dd249
drichards6 <drr5489@psu.edu>
2024-02-29 13:19:46 -0500

    Worked on design for alerts (got slants)

--- src/alerts.js
+++ src/alerts.js
@@ -9,51 +9,98 @@ export class Alerts extends LitElement {
   constructor() {
       super();
       this.sticky = false;
-      this.urgency = "warning"
+      this.status = "warning"
+      this.opened = true;
+      if (localStorage.getItem('campus-alert-opened-state') == 'false') {
+        this.opened = false;
+      }
+      this.msg = "This is a warning. Exercise caution."
+      this.date = "August 22, 2024"
     } 
   
   static get styles() {
       return css`
         :host {
-          display: block;
+          display: flex;
           width: 100%;
           height: 128px;
-          background-color: grey;
         }
 
-        :host([sticky]) {
-            // do something 
+        :host([status="notice"]) {
+            --outsidebgcolor: blue;
+            --insidebgcolor: #5b99e5;
         }
 
-        :host([urgency="notice"]) {
-            background-color: lightblue; //make this assign the variable color, not change the background color
+        :host([status="warning"]) {
+            --outsidebgcolor: yellow;
+            --insidebgcolor: #e1bc29;
         }
 
-        :host([urgency="warning"]) {
-            background-color: yellow;
+        :host([status="alert"]) {
+            --outsidebgcolor: red;
+            --insidebgcolor: #a70303;
         }
 
-        :host([urgency="alert"]) {
-            background-color: red;
+        .outside-wrapper {
+          display: flex;
+          background-color: var(--outsidebgcolor);
+          height: 100%;
+          width: 100%;
+          align-items: center;
+          justify-content: center;
+          position: relative;
+        }   
+
+        button {
+          max-height: 24px;
+          position: absolute;
+          top: 10%;
+          right: 20%;
+          background-color: white; 
+        }
+
+        .middle-wrapper {
+          transform: skew(20deg);
+          display: flex;
+          background-color: var(--insidebgcolor);
+          height: 100%;
+          align-items: center;
+          justify-content: center;
+          padding: 0 24px;
+        }
+
+        .text-wrapper {
+          transform: skew(-20deg);
+          display: flex;
+          align-items: center;
+          justify-content: center;
         }
 
       `;
     }
 
-    
+  toggleAlert() {
+    this.opened = !this.opened;
+  }
+
   render() {
       return html`
-        <div class="alert"> 
-            <h1>Alert</h1>
+        <div class="outside-wrapper"> 
+          <div class="middle-wrapper"> 
+            <p class="text-wrapper">This is a larger alert message.</p>
+          </div>  
+            <button>x</button>
         </div>
+
       `; 
     }
   
   static get properties() {
       return {
         sticky: { type: Boolean, reflect: true },
-        urgency: { type: String },
-
+        status: { type: String, reflect: true },
+        opened: { type: Boolean, reflect: true},
+        msg: { type: String },
       };
     }
 }
