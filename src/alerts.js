ad0d6eb47fef419876d4f572b0e57325533dcdd1
drichards6 <drr5489@psu.edu>
2024-03-19 12:39:20 -0400

    Scrapped previous work, attempted to start again

--- src/alerts.js
+++ src/alerts.js
@@ -15,8 +15,9 @@ export class Alerts extends LitElement {
         this.opened = false;
         this.style.setProperty('--custom-alert-height', '48px');
       }
-      this.msg = "This is a warning. Exercise caution."
-      this.date = "August 22, 2024"
+      this.msg = "This is a warning. Exercise caution.";
+      this.date = "August 22, 2024";
+      this.time = "3:00 PM";
     } 
   
   static get styles() {
@@ -30,7 +31,7 @@ export class Alerts extends LitElement {
         max-height: var(--custom-alert-height);
       }
 
-        :host([sticky]) {
+        .sticky {
           position: sticky;
           top: 0;
           z-index: 99999;
@@ -91,6 +92,34 @@ export class Alerts extends LitElement {
           max-width: 540px;
         }
 
+        .alert-icon {
+            transform: skew(-20deg) scale(1.25); 
+            margin-left: 4vw;
+            flex: 0 0 auto;
+        }
+
+        .datetime {
+            text-align: left;
+            margin-left: 6vw;
+            margin-right: 6vw;
+            font-weight: 600;
+            font-size: 90%;
+            text-transform: uppercase;
+        }    
+
+        .triangle {
+            border-style: solid;
+            border-width: 0 50px 50px;
+            width: 0;
+            height: 0;
+            position: absolute;
+            bottom: 2rem;
+            left: -2rem;
+            border-left: 35px solid transparent;
+            border-right: 0px solid transparent;
+            color: var(--message-background-color);
+        }
+
       `;
     }
 
@@ -108,19 +137,38 @@ export class Alerts extends LitElement {
 
   }
 
-  render() {
-      return html`
-        <div class="outside-wrapper ${this.open ? '' : 'closed'}" ?sticky="${this.sticky}"> 
-          <div class="middle-wrapper"> 
-            <div class="text-wrapper"><p><slot name="title"></slot></p>
-            ${this.open ? html`<slot></slot>` : ''}</div>
-          </div>  
-            <button class="btn" @click="${this.toggleAlert}">
-              ${this.open ? 'Close' : 'Open'} 
-            </button>
+  openedAlert() {
+    return html`
+      <div class="outside-wrapper"> 
+        <slot>
+          <div class='datetime'>
+            <p class='date'>${this.date}</p>
+            <p class='time'>${this.time}</p>
+          </div>
+        <div class="middle-wrapper"> 
+          <!-- icon, paragraph, triangle wedge-->
+          <svg class="alert-icon" xmlns="http://www.w3.org/2000/svg" style="height: 90px; width: 90px; align-items: center;" viewBox="0 0 24 24"><title>alert-circle-outline</title><path d="M11,15H13V17H11V15M11,7H13V13H11V7M12,2C6.47,2 2,6.5 2,12A10,10 0 0,0 12,22A10,10 0 0,0 22,12A10,10 0 0,0 12,2M12,20A8,8 0 0,1 4,12A8,8 0 0,1 12,4A8,8 0 0,1 20,12A8,8 0 0,1 12,20Z"/></svg>
+          <p class="msg-text"></p>
+          <div class="triangle">${this.msg}</div>
         </div>
+        <div class='btn-wrapper'>
+          <button class="btn" @click="${this.toggleAlert}"></button>
+        </div>
+      </slot>
+      </div>
 
-      `; 
+    `;
+  }
+
+  closedAlert() {
+    return html`
+
+
+    `;
+  }
+
+  render() {
+      return (this.openedAlert());
     }
   
   static get properties() {
